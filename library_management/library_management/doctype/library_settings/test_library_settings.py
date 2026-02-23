4a9785a6414c299cd957ca9984e9c4ecdb52ae56
Rahul PA <rahulmeppadi@gmail.com>
2026-02-23 11:31:43 +0530

    Added updated workspaces from pearl-erp.com

---
+++ library_management/library_management/doctype/library_settings/test_library_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, admin and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLibrarySettings(FrappeTestCase):
+	pass
